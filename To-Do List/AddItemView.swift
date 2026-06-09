3fc9530d1faae144dd8489823bf2af8fe926d28a
Pam VanLeirsburg <pvanleirsburg@barrington220.org>
2026-06-09 14:04:42 -0500

    Created AddItemView

---
+++ To-Do List/AddItemView.swift
@@ -0,0 +1,18 @@
+//
+//  AddItemView.swift
+//  To-Do List
+//
+//  Created by Pamela VanLeirsburg on 6/9/26.
+//
+
+import SwiftUI
+
+struct AddItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddItemView()
+}
