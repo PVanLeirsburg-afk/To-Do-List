4698a223f7a43920bf366ce10b5c790e7fba4b13
Pam VanLeirsburg <pvanleirsburg@barrington220.org>
2026-06-09 13:13:16 -0500

    Initial Commit

---
+++ To-Do List/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  To-Do List
+//
+//  Created by Pamela VanLeirsburg on 6/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ To-Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To-Do List
+//
+//  Created by Pamela VanLeirsburg on 6/9/26.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
